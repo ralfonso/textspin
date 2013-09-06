4178546d33a43174be2e47f9aa39aa35d4f930fa
Ryan Roemmich <ralfonso@gmail.com>
2013-09-06 10:19:57 -0600

    comments, add GH link

--- app/scripts/spin.js
+++ app/scripts/spin.js
@@ -2,6 +2,7 @@
 
 textspin.controller('SpinCtrl', ['$scope', '$routeParams', '$location', '$timeout', '$window',
     function($scope, $routeParams, $location, $timeout, $window) {
+        // set some default params and lols
         var defaultSpeed = 50;
         var defaultArms = 9;
         var randomLols = ['DUCKTALES',
@@ -23,16 +24,22 @@ textspin.controller('SpinCtrl', ['$scope', '$routeParams', '$location', '$timeou
                           'PUBLIC MILK',
                           'POKOENCEPHELON',
                           ':)))))))>><><>@<>@<--()----'];
+
+        // initialize our spiner obj. we use an intermediate object
+        // to allow properties to trickle down to inner scopes
         $scope.spinner = {};
 
         var init = function() {
+            // attempt to load values from the b64 encoded url
             var spinText = $routeParams.spinText;
             var spinArms = $routeParams.spinArms;
             var spinSpeed = $routeParams.spinSpeed;
 
+            // FIXME: there has to be a better way to parse/validate these
             $scope.spinner.text = spinText ? Base64.decode(spinText) : _.first(_.shuffle(randomLols));
             $scope.spinner.arms = spinArms ? parseInt(Base64.decode(spinArms), 10) : defaultArms;
             $scope.spinner.speed = spinArms ? parseInt(Base64.decode(spinSpeed), 10) : defaultSpeed;
+
 
             if (!_.isNumber($scope.spinner.arms) || _.isNaN($scope.spinner.arms)) {
                 $scope.spinner.arms = defaultArms;
@@ -43,12 +50,15 @@ textspin.controller('SpinCtrl', ['$scope', '$routeParams', '$location', '$timeou
             }
 
             $scope.spinner.armsList = _.range(0, $scope.spinner.arms);
+
             if (spinText && spinArms) {
                 $scope.url = $window.location;
             }
         };
 
         var convertSpeed = function(speed) {
+            // janky logarithmic scale to convert our user-friendly 1-100
+            // scale to a millisecond delay for the CSS3 animation
             var minSpeed = 1;
             var maxSpeed = 100;
             var minVal = Math.log(12000);
@@ -58,6 +68,7 @@ textspin.controller('SpinCtrl', ['$scope', '$routeParams', '$location', '$timeou
         };
 
         $scope.craft = function() {
+            // b64 is for obfuscators. called when the save button is smashed
             var b64Text = Base64.encode($scope.spinner.text);
             var b64Arms = Base64.encode($scope.spinner.arms.toString());
             var b64Speed = Base64.encode($scope.spinner.speed.toString());
@@ -65,6 +76,7 @@ textspin.controller('SpinCtrl', ['$scope', '$routeParams', '$location', '$timeou
         };
 
         $scope.armStyle = function(index) {
+            // applies a css style to each arm of the spinner
             var div = 360 / $scope.spinner.arms;
             var rotStart = div * index;
             var rotStop = rotStart + 360;
@@ -76,11 +88,17 @@ textspin.controller('SpinCtrl', ['$scope', '$routeParams', '$location', '$timeou
         };
 
         var redraw = function() {
-            $scope.spinner.armsList = [];
+            // removes and redraws the spinner. abuses the fact that
+            // the displayed spinner is actually driven by $scope.armsList,
+            // not $scope.arms
 
             // FIXME: forgive me for doing it this way, but we need
             // a redraw of the elements so the animations restart
+            $scope.spinner.armsList = [];
+
             $timeout(function() {
+                // add a small delay and set the armsList, which will
+                // cause the spinner to be redrawn
 
                 // FIXME: this introduces a race condition
                 if (_.isUndefined($scope.spinner.speed) || _.isNaN($scope.spinner.speed)) {
@@ -92,9 +110,11 @@ textspin.controller('SpinCtrl', ['$scope', '$routeParams', '$location', '$timeou
 
         };
 
+        // FIXME: find a way to do this without explicit watches
         $scope.$watch('spinner.arms', redraw);
         $scope.$watch('spinner.speed', redraw);
 
+        // I guess it's ok to initialize like this in angularjs
         init();
     }
 ]);
